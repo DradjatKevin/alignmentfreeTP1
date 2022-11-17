43e4bb169c233ec5bbf7bddb4fe468ec5dc25f70
DradjatKevin <98811563+DradjatKevin@users.noreply.github.com>
2022-11-17 18:28:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,46 @@ from kmers import stream_kmers, kmer2str
 
 
 def similarity(A, inter, B):
-    # --- To complete ---
-    pass
+    return inter / (A+inter), inter / (B+inter)
 
 
 def jaccard(A, inter, B):
-    # --- To complete ---
-    pass
+    return inter / (A + B +inter)
 
 
+def intersection(A, B) :
+    dict_1, dict_2 = {}, {}
+    inter_kmers = []
+    inter_A, inter_B = 0, 0
+    inter_kmers = set()
+    
+    for kmer in A :
+        if kmer not in dict_1 :
+            dict_1[kmer] = 1
+        else :
+            dict_1[kmer] += 1
+    
+    for kmer in B :
+        if kmer in dict_1 :
+            inter_kmers.add(kmer)
+            if kmer not in dict_2 :
+                dict_2[kmer] = 1
+            else :
+                dict_2[kmer] += 1
+                
+    inter_kmers = set(inter_kmers)
+    
+    for kmer in inter_kmers :
+        inter_A += dict_1[kmer]
+        inter_B += dict_2[kmer]
+    
+    return inter_A, inter_B
+
+
+def my_method(A, B) :
+    inter_A, inter_B = intersection(A, B)
+    print(inter_A, inter_B)
+    return len(A)-inter_A, inter_A+inter_B, len(B)-inter_B
 
 if __name__ == "__main__":
     # Load all the files in a dictionary
@@ -20,10 +51,18 @@ if __name__ == "__main__":
     k = 21
     
     filenames = list(files.keys())
+    
+    # Stream
+    streamed_kmers = []
+    for filename in filenames :
+        kmers = []
+        for i in files[filename] :
+            kmers.extend(stream_kmers(i, k))
+        files[filename] = kmers
+        
+    
     for i in range(len(files)):
         for j in range(i+1, len(files)):
-            
-            # --- Complete here ---
 
-            A, inter, B = my_method(indexes[filenames[i]], files[filenames[j]], k)
+            A, inter, B = my_method(files[filenames[i]], files[filenames[j]])
             print(filenames[i], filenames[j], jaccard(A, inter, B), similarity(A, inter, B))
