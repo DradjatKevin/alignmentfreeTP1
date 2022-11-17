35afd29516df6882a88845f17e3b05516071cd21
DradjatKevin <98811563+DradjatKevin@users.noreply.github.com>
2022-11-17 18:23:19 +0100

    Update kmers.py

--- kmers.py
+++ kmers.py
@@ -16,5 +16,32 @@ def kmer2str(val, k):
 
 
 def stream_kmers(text, k):
-    # --- To complete ---
-    pass
+    list_kmer = []
+    kmer = 0
+    reverse_kmer = 0
+    
+    for i in range(k) :
+        kmer = kmer << 2
+        kmer += encode(text[i])
+        reverse_kmer += (3 - encode(text[i])) * (2**(2*i))
+    list_kmer.append(min(kmer, reverse_kmer))
+
+    mask = (1<<(k-1)*2)-1
+    for n in text[k:] :
+        kmer = kmer & mask
+        kmer = kmer << 2
+        kmer = kmer + encode(n)
+                         
+        reverse_kmer = reverse_kmer >> 2
+        reverse_kmer += (3 - encode(n)) * (2**(2*k))
+        list_kmer.append(min(kmer, reverse_kmer))
+        
+    return list_kmer
+
+
+def encode(x) :
+    dico = {'A':0, 'C':1, 'T':2, 'G':3}
+    if x in dico.keys() :
+        return dico[x]
+    else :
+        return 0
