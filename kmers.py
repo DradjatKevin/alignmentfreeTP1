6df049c4fb9db0054dea458300f418d008b7b2fe
DradjatKevin <98811563+DradjatKevin@users.noreply.github.com>
2022-11-27 22:30:11 +0100

    Update kmers.py

--- kmers.py
+++ kmers.py
@@ -1,10 +1,7 @@
+import heapq 
+
 
 def kmer2str(val, k):
-    """ Transform a kmer integer into a its string representation
-    :param int val: An integer representation of a kmer
-    :param int k: The number of nucleotides involved into the kmer.
-    :return str: The kmer string formatted
-    """
     letters = ['A', 'C', 'T', 'G']
     str_val = []
     for i in range(k):
@@ -16,32 +13,86 @@ def kmer2str(val, k):
 
 
 def stream_kmers(text, k):
-    list_kmer = []
+    # Precompute the (k-1)-mer (and reverse)
     kmer = 0
-    reverse_kmer = 0
-    
-    for i in range(k) :
-        kmer = kmer << 2
-        kmer += encode(text[i])
-        reverse_kmer += (3 - encode(text[i])) * (2**(2*i))
-    list_kmer.append(min(kmer, reverse_kmer))
+    rkmer = 0
+    for letter in text[:k-1]:
+        # A = 00, C = 01, T = 10, G = 11
+        # Forward kmer
+        kmer <<= 2
+        letter_value = (ord(letter) >> 1) & 0b11
+        kmer += letter_value
+        # Reverse kmer
+        rkmer >>= 2
+        rev_letter_value = (letter_value + 2) & 0b11
+        rkmer += rev_letter_value << (2 * (k - 1))
 
-    mask = (1<<(k-1)*2)-1
-    for n in text[k:] :
-        kmer = kmer & mask
-        kmer = kmer << 2
-        kmer = kmer + encode(n)
-                         
-        reverse_kmer = reverse_kmer >> 2
-        reverse_kmer += (3 - encode(n)) * (2**(2*k))
-        list_kmer.append(min(kmer, reverse_kmer))
+    # Stream kmers
+    mask = (1 << (2 * k)) - 1
+    for letter in text[k-1:]:
+        # Forward kmer
+        kmer <<= 2
+        letter_value = (ord(letter) >> 1) & 0b11
+        kmer += letter_value
+        kmer &= mask
+        # Reverse kmer
+        rkmer >>= 2
+        rev_letter_value = (letter_value + 2) & 0b11
+        rkmer += rev_letter_value << (2 * (k - 1))
+
+        yield kmer, rkmer
+
+
+
         
-    return list_kmer
+def sketch(s, stream, seq, k) :
+    L = []
+    for kmer, rkmer in stream(seq, k) :
+        Kmer = min(kmer, rkmer)
+        Kmer = xorshift(Kmer)
+        
+        if len(L) < s :
+            L.append(Kmer)
+        else :
+            Max = max(L)
+            if Kmer > Max :
+                index_max = L.index(Max)
+                L[index_max] = Kmer
+                
+    return sorted(L)
 
 
-def encode(x) :
-    dico = {'A':0, 'C':1, 'T':2, 'G':3}
-    if x in dico.keys() :
-        return dico[x]
-    else :
-        return 0
+
+
+def sketch_queue(s, stream, seq, k) :
+    L = []
+    
+    for kmer, rkmer in stream(seq, k) :
+        Kmer = min(xorshift(kmer), xorshift(rkmer))
+        if len(L) < s :
+            L.append(Kmer)
+            if len(L) == s :
+                heapify(L)
+        else :
+            Max = maxheap(L)
+            if Kmer > Max :
+                heappop(L)
+                heappush(L, Kmer)
+                
+    return sorted(L)
+            
+
+    
+def xorshift(x) :
+    x ^= x << 13;
+    x ^= x >> 7;
+    x ^= x << 17;
+    return x
+
+
+def hash(list_kmers) :
+    return [xorshift(i) for i in list_kmers]
+
+
+print(sketch(100, stream_kmers, 'CGGAATTA', 3))
+            
